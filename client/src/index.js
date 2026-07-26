af9d4697a1e6edcb965f259279f4809042994450
Friedrick2003 <himanshukumar151821@gmail.com>
2026-07-26 12:10:34 +0530

    fix: hardcode google client id as fallback for vercel

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || "missing_client_id"}>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || "316696097984-hb5ctdt8spesioir3jqjml9s5r0ndth3.apps.googleusercontent.com"}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
